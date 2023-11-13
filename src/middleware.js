2d64c0f0d7767662fd7469324ca68e3b90f71cff
Suphakit P <me@suphakit.net>
2023-11-14 00:57:15 +0700

    feat<middleware>: setting up body-parser

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const corsOptions = {
   origin: '*',
@@ -7,6 +8,13 @@ const corsOptions = {
 
 async function Middleware(app, exp, root) {
   app.use(cors(corsOptions))
+
+  app.use(bodyParser.json())
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
 
   app.use('/statics', exp.static(root + '/statics'))
 }
